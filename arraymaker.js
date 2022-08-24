b773a8efc675c48ff17ec6eefc276e4f46121f6a
twufu <tntspys2@gmail.com>
2022-08-24 15:41:09 +0100

    hiiiiii ^-^

---
+++ arraymaker.js
@@ -0,0 +1,13 @@
+import "./break_infinity.js";
+var array = {}
+var array2 = {}
+var basePrice = new Decimal(10)
+var expo = new Decimal(1.25)
+var currentOwned = new Decimal(0)
+var money = 25
+
+for (let i = 0; i < 100; i++) {
+    array[i] = Decimal.sumGeometricSeries(i*25,basePrice, expo, currentOwned)
+}
+
+console.log(array)
---
+++ main.js
@@ -0,0 +1,65 @@
+var cookies = 0;
+var cursors = 0;
+var lean = 0;
+
+function click(number){
+    cookies = cookies + number*Math.pow(2, lean);
+    console.log(cookies)
+    document.getElementById("cookies").innerHTML = cookies;
+};
+
+function save(){
+    var savegame = {
+        cookies: cookies,
+        cursors: cursors,
+        lean: lean
+    }
+
+    localStorage.setItem("save",JSON.stringify(savegame));
+}
+
+window.onload = function load(){
+    var savegame = JSON.parse(localStorage.getItem("save"));
+    if (typeof savegame.cookies !== "undefined") cookies = savegame.cookies;
+    if (typeof savegame.cursors !== "undefined") cursors = savegame.cursors;
+    if (typeof savegame.lean !== "undefined") lean = savegame.lean;
+}
+
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));  
+    if(cookies >= cursorCost){                                
+        cursors = cursors + 1;
+    	cookies = cookies - cursorCost;
+        document.getElementById('cursors').innerHTML = cursors; 
+        document.getElementById('cookies').innerHTML = cookies;
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));   
+    document.getElementById('cursorCost').innerHTML = nextCost;  
+};
+
+function buyLean(){
+    var leanCost = Math.floor(300 * Math.pow(3,lean));     
+    if(cookies >= leanCost){                                  
+        lean = lean + 1;                                
+    	cookies = cookies - leanCost;                         
+        document.getElementById('lean').innerHTML = lean;  
+        document.getElementById('cookies').innerHTML = cookies; 
+    };
+    var nextCost = Math.floor(300 * Math.pow(3,lean));      
+    document.getElementById('leanCost').innerHTML = nextCost;  
+};
+
+window.setInterval(function(){
+
+	click(cursors)
+    save()
+
+    document.getElementById('cursors').innerHTML = cursors;
+    document.getElementById('cookies').innerHTML = cookies;
+    document.getElementById('lean').innerHTML = lean;
+    document.getElementById('cursorCost').innerHTML = Math.floor(10 * Math.pow(1.1,cursors));
+    if (lean > 1) {
+        document.getElementById('leanCost').innerHTML = Math.floor(300 * Math.pow(3,lean)); 
+    }
+
+}, 1000);
