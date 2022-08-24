31b2eda27c004b8236edde6f141fefd5a831eabb
twFJUFUUHAHAHAHAHAHHAHAbhh <29336231+twufu@users.noreply.github.com>
2022-08-24 14:52:58 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-var stone = 0;
-var picks = 0;
+var cookies = 0;
+var cursors = 0;
 var lean = 0;
 
-function mine(number){
-    stone = stone + number*(Math.pow(2,lean+1)/2);
-    document.getElementById("stone").innerHTML = stone;
+function click(number){
+    cookies = cookies + number*Math.pow(2, lean);
+    document.getElementById("cookies").innerHTML = cookies;
 };
 
 function save(){
     var savegame = {
-        stone: stone,
-        picks: picks,
+        cookies: cookies,
+        cursors: cursors,
         lean: lean
     }
 
@@ -19,20 +19,50 @@ function save(){
 
 window.onload = function load(){
     var savegame = JSON.parse(localStorage.getItem("save"));
-    if (typeof savegame.stone !== "undefined") stone = savegame.stone;
-    if (typeof savegame.picks !== "undefined") picks = savegame.picks;
+    if (typeof savegame.cookies !== "undefined") cookies = savegame.cookies;
+    if (typeof savegame.cursors !== "undefined") cursors = savegame.cursors;
     if (typeof savegame.lean !== "undefined") lean = savegame.lean;
 }
 
-function max(x,y,z){ // x is current cost, y is what the cost is multiplied by, z is how much currency you have
-    x = x * y
-    if(z >= x){
-        return [x, 1]
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));  
+    if(cookies >= cursorCost){                                
+        cursors = cursors + 1;
+    	cookies = cookies - cursorCost;
+        document.getElementById('cursors').innerHTML = cursors; 
+        document.getElementById('cookies').innerHTML = cookies;
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));   
+    document.getElementById('cursorCost').innerHTML = nextCost;  
+};
+
+function buyLean(){
+    var leanCost = Math.floor(300 * Math.pow(3,lean));     
+    if(cookies >= leanCost){                                  
+        lean = lean + 1;                                
+    	cookies = cookies - leanCost;                         
+        document.getElementById('lean').innerHTML = lean;  
+        document.getElementById('cookies').innerHTML = cookies; 
+    };
+    var nextCost = Math.floor(300 * Math.pow(3,lean));      
+    document.getElementById('leanCost').innerHTML = nextCost;  
+};
+
+window.setInterval(function(){
+
+	click(cursors)
+    save()
+
+    document.getElementById('cursors').innerHTML = cursors;
+    document.getElementById('cookies').innerHTML = cookies;
+    document.getElementById('lean').innerHTML = lean;
+    document.getElementById('cursorCost').innerHTML = Math.floor(10 * Math.pow(1.1,cursors));
+    if (lean > 1) {
+        document.getElementById('leanCost').innerHTML = Math.floor(300 * Math.pow(3,lean)); 
     }
 
-}
+}, 1000);
 
-function buyPick(){
     var pickCost = Math.floor(10 * Math.pow(1.1,picks));  
     if(stone >= pickCost){                                
         picks = picks + 1;
